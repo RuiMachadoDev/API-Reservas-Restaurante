35ead40efca1411f75f6acdf33177a441c1e4238
RuiMachadoDev <ruifilipemachado@hotmail.com>
2025-01-22 12:33:09 +0000

    Atualização das migrações da BD

---
+++ reservations-api/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'owner',
+        ]);
+
+        User::create([
+            'name' => 'Customer User',
+            'email' => 'customer@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'customer',
+        ]);
+    }
+}
+
